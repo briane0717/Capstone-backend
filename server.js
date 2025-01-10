064397a29ac26ed6ecec8cec8a10f22e1584c512
Brian <briane0717@gmail.com>
2025-01-10 16:48:17 -0500

    connected to server

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ import logger from "morgan";
 import cors from "cors";
 
 dotenv.config();
+
+// Connect to MongoDB
+import db from "./db/conn.mjs";
+//Set up port
+const PORT = process.env.PORT || 5050;
+
+//Create app
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
